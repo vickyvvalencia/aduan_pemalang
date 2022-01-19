82cce8e6df77b0071312a92863a54bc2be568ba2
vickyvvalencia <92201397+vickyvvalencia@users.noreply.github.com>
2022-01-19 08:34:14 +0700

    Add files via upload

---
+++ Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function pengaduan()
+    {
+        return view('Admin.pengaduan');
+    }
+
+    
+    public function detail_pengaduan()
+    {
+        return view('Admin.detail_pengaduan');
+    }
+
+    
+    public function daftar_pengaduan()
+    {
+        return view('Admin.daftar_pengaduan');
+    }
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
